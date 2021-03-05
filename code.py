43f86437fe3a6fbcc861202ecce14b39bc725db8
Daniel Soto <sotod@sonoma.edu>
2021-03-04 17:13:49 -0800

    adds rpm and motor current reads

--- code.py
+++ code.py
@@ -13,25 +13,51 @@ vehicle_data = {'battery_voltage':0,
                 'high_battery_temp':0,
                 'high_BMS_temp':0,
                 'motor_rpm':0,
+                'total_current':0,
                 'motor_temperature':0,
                 'motor_current':0,
                 'controller_temperature':0}
+
+vehicle_data_last_read = {'battery_voltage':0,
+                          'battery_current':0,
+                          'battery_voltage_BMS':0,
+                          'battery_current_BMS':0,
+                          'high_cell_voltage':0,
+                          'low_cell_voltage':0,
+                          'high_battery_temp':0,
+                          'high_BMS_temp':0,
+                          'motor_rpm':0,
+                          'total_current':0,
+                          'motor_temperature':0,
+                          'motor_current':0,
+                          'controller_temperature':0}
 
 print("ENNOID/VESC CAN reader")
 
 def print_to_console():
 
     print()
+    print("R", vehicle_data['motor_rpm']/1E0, end=' | ')
     print("H", vehicle_data['high_cell_voltage']/1E5, end=' | ')
     print("L", vehicle_data['low_cell_voltage']/1E5, end=' | ')
     print('Vb', vehicle_data['battery_voltage_BMS']/1E5, end=' | ')
     print('Vc', vehicle_data['battery_voltage']/1E1, end=' | ')
     print('I', vehicle_data['battery_current_BMS']/1E5, end=' | ')
+    print('Im', vehicle_data['total_current']/1E1, end=' | ')
     print('Tc', vehicle_data['controller_temperature']/1E1, end=' | ')
     print('Tm', vehicle_data['motor_temperature']/1E1, end=' | ')
     print(time.monotonic())
 
 def update_parameters():
+
+    # currently updates variables every time a packet is encountered
+    # will need to modify to get deltas
+    # or maybe filter and then derived_fsm samples?
+    # this could be a dict of dicts specifying variable code, byte start, byte length, and variable string
+
+    def process_vesc_09XX(vehicle_data):
+        vehicle_data['motor_rpm'] = struct.unpack('>L', message.data[0:4])[0]
+        vehicle_data['total_current'] = struct.unpack('>H', message.data[4:6])[0]
 
     def process_vesc_10XX(vehicle_data):
         vehicle_data['controller_temperature'] = struct.unpack('>H', message.data[0:2])[0]
@@ -47,10 +73,12 @@ def update_parameters():
         vehicle_data['battery_voltage_BMS'], vehicle_data['battery_current_BMS'] = struct.unpack('>ii', message.data)
 
     # TODO: make CAN ids based on device ID config file
+    # TODO: more robust indexing scheme
 
-    message_ids = [0x1001, 0x1b01, 0x1e0a, 0x1f0a]
+    message_ids = [0x0901, 0x1001, 0x1b01, 0x1e0a, 0x1f0a]
 
-    read_functions = {'0x1001':process_vesc_10XX,
+    read_functions = {'0x901':process_vesc_09XX,
+                      '0x1001':process_vesc_10XX,
                       '0x1b01':process_vesc_1bXX,
                       '0x1e0a':process_dbms_1eXX,
                       '0x1f0a':process_dbms_1fXX}
@@ -76,6 +104,9 @@ bus = can.interface.Bus(bustype='slcan',
 
 display_update_seconds = 1.0
 last_display = time.monotonic()
+
+# display class just checks if time to print and prints
+# class CONSOLE():
 
 while 1:
     update_parameters()
